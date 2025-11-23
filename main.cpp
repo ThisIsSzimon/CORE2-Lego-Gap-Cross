4edd8b4f4aa70aafa071782b3580e07e18c0cc09
Szymon Kotuła <szymon.kotula@gmail.com>
2025-11-23 19:17:06 +0100

    Sterowanie ręcznie - program

--- main.cpp
+++ main.cpp
@@ -1,47 +1,65 @@
 #include <hFramework.h>
-#include <DistanceSensor.h>
 
-using namespace hModules;
+const int VEH_BRIDGE_POWER  = 500;  // moc dla pojazdu na moście (hMot4)
+const int VEH_DESK_POWER    = 500;  // moc dla pojazdu na biurku (hMot1)
 
 void hMain()
 {
+    // UART na złączu hExt (tu wpina się ESP32)
     hExt.serial.init(115200);
-    char c;
-    while (1)
+
+    // Na start zatrzymujemy wszystkie silniki
+    hMot1.setPower(0);
+    hMot2.setPower(0);
+    hMot3.setPower(0);
+    hMot4.setPower(0);
+
+    Serial.printf("CORE2: start, oczekuje komend z ESP32...\r\n");
+
+    for (;;)
     {
-        if (hExt.serial.available() > 0) { // checking Serial availability
-            c = hExt.serial.getch();
-            if(c == 'a') {
-                hLED1.on();
-                hLED2.off();
-                hLED3.off();
-                hMot1.setPower(500);
-            }
-            if(c == 's') {
-                hLED1.off();
-                hLED2.on();
-                hLED3.off();
-                hMot1.setPower(0);
-            }
-            if(c == 'd') {
-                hLED1.off();
-                hLED2.off();
-                hLED3.on();
-                hMot1.setPower(-500);
-            }
-            if(c == 'z'){
+        char cmd;
+
+        if (hExt.serial.read(&cmd, 1, 10))
+        {
+            Serial.printf("CORE2: cmd = %c\r\n", cmd);
+
+            switch (cmd)
+            {
+            // ================= MOST =================
+            case 'A':
+                hMot2.setPower(-900);
+                hMot3.setPower(-1000);
+                break;
+
+            case 'a':
                 hMot2.setPower(1000);
                 hMot3.setPower(1000);
+                break;
 
-            }
-            if(c == 'x'){
-                hMot2.setPower(0);
-                hMot3.setPower(0);
-            }
-            if(c == 'c'){
-                hMot2.setPower(-1000);
-                hMot3.setPower(-1000);
+            // =========== POJAZD NA MOŚCIE ===========
+            case 'B':
+                hMot4.setPower(VEH_BRIDGE_POWER);
+                break;
+
+            case 'b':
+                hMot4.setPower(-VEH_BRIDGE_POWER);
+                break;
+
+            // ========== POJAZD NA BIURKU ============
+            case 'C':
+                hMot1.setPower(VEH_DESK_POWER);
+                break;
+
+            case 'c':
+                hMot1.setPower(-VEH_DESK_POWER);
+                break;
+
+            default:
+                break;
             }
         }
+
+        sys.delay(5);
     }
 }
